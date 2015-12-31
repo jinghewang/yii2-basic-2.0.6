f6f264cc7fad1ee91a4532bd4c3451701093e444
jinghewang <jinghewang@163.com>
2015-12-31 14:02:49 +0800

    index3

---
+++ composer.bat
@@ -0,0 +1,9 @@
+安装依赖
+
+安装依赖非常简单，只需在项目目录下运行：
+
+composer install
+
+如果没有全局安装的话，则运行：
+
+php composer.phar install
--- web/index.php
+++ web/index.php
@@ -8,7 +8,7 @@ require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 //define
-require(__DIR__ . '/../vendor/yiisoft/hlt/GlobalFunction.php');
+//require(__DIR__ . '/../vendor/yiisoft/hlt/GlobalFunction.php');
 
 $config = require(__DIR__ . '/../config/web.php');
 
